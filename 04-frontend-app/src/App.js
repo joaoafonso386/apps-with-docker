6154786e2335992c2731f93262a38721d071f27c
João Afonso <joaoafonso386@gmail.com>
2024-01-24 23:31:30 +0000

    Update app to pass tests

--- 04-frontend-app/src/App.js
+++ 04-frontend-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Trigger Travis CI!
+          Trigger Travis CI! Learn react
         </a>
       </header>
     </div>
